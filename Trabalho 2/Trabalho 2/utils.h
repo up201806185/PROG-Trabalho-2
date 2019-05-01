3ed9a0ca745599315c80d5791b9acd99ce61b684
up201806185 <up201806185@up201806185.(none)>
2019-05-01 14:29:12 +0100

    Added some utils functions
    utils::read_str
    utils::trim
    utils::split

--- Trabalho 2/Trabalho 2/utils.h
+++ Trabalho 2/Trabalho 2/utils.h
@@ -3,11 +3,17 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
 
 namespace utils
 {
+	//Input handling
 	bool read_str(std::istream & stream, std::string & input, bool read_only_one_string = false);
 
+
+	//Usefull string operations
+	void trim(std::string & input);
+	std::vector<std::string> split(std::string input, const char delimiter, size_t max_splits = -1); // -1 will make it loop back to its maximum value
 }
 
 #endif
