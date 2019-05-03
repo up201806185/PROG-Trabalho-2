44d5f58e3b89dbcc80fc49385b00c53b3f2a03d0
up201806185 <up201806185@up201806185.(none)>
2019-05-03 14:53:41 +0100

    Definition of the header

--- Trabalho 2/Trabalho 2/Date.h
+++ Trabalho 2/Trabalho 2/Date.h
@@ -1,11 +1,38 @@
-#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <string>
+#include <istream>
 class Date
 {
+//friend:
+//<< operator
+//== operator
+//< operator
+//> operator
 public:
 	Date();
+	Date(std::istream & stream);
+	Date(std::string input);
+	Date(unsigned short int day, unsigned short int month, unsigned short int year);
+
 	~Date();
+
+	bool set_day(const unsigned short int day);
+	bool set_month(const unsigned short int month);
+	bool set_year(const unsigned short int year);
+	bool parse(std::istream & stream);
+	bool parse(std::string input);
+	void set_today();
+
+	void get_day();
+	void get_month();
+	void get_year();
+	std::string get_date();
 private:
+	unsigned short int days_in_month();
+	unsigned short int days_in_feb();
+	void reset_date();
 	std::string error_message;
 	bool is_valid;
 	unsigned short int day;
@@ -27,3 +54,5 @@ tm currentDate(time_t timer) {
 	return bt;
 }
 */
+
+#endif
