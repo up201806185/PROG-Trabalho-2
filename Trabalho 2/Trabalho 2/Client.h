49bb241f099bc94839f4de6676908fc02cbacf46
up201806330 <45789887+up201806330@users.noreply.github.com>
2019-05-04 22:20:06 +0100

    Updated Client.h

--- Trabalho 2/Trabalho 2/Client.h
+++ Trabalho 2/Trabalho 2/Client.h
@@ -7,34 +7,35 @@
 
 class Client
 {
-	friend ostream& operator<<(ostream& out, const Client & client);
+	friend std::ostream& operator<<(std::ostream& out, const Client & client);
 
 public:
+	Client();
 	Client(std::string name, size_t nif, unsigned short f_size, Address address);
-	Client(std::string name, size_t nif, unsigned short f_size, Address address, std::vector<TravelPack> &packs, size_t total_purchased);
+	Client(std::string name, size_t nif, unsigned short f_size, Address address, std::vector<Travelpack> &packs, size_t total_purchased);
 
 	~Client();
 
-	std::string get_name() const;
-	size_t get_nif() const;
-	unsigned short get_f_size() const;
-	Address get_address() const;
-	std::vector<TravelPack> get_packs() const;
-	size_t get_total_purchased() const;
+	std::string get_name();
+	size_t get_nif();
+	unsigned short get_f_size();
+	Address get_address();
+	std::vector<Travelpack> get_packs();
+	size_t get_total_purchased();
 
 	void set_name(std::string name);
-	void set_nif(unsigned nif);
+	void set_nif(size_t nif);
 	void set_f_size(unsigned short f_size);
 	void set_address(Address address);
-	void set_packs(vector<TravelPack> &packs);
-	void setTotalPurchased(unsigned total_purchased);
+	void set_packs(std::vector<Travelpack> &packs);
+	void set_total_purchased(size_t total_purchased);
 
 private:
 	std::string name;
 	size_t nif;
 	unsigned short f_size;
 	Address address;
-	std::vector <TravelPack> packs;
+	std::vector <Travelpack> packs;
 	size_t total_purchased;
 };
 
