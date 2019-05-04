d2e8474b6c78202ea910d8b0cb3980722275206c
up201806185 <up201806185@up201806185.(none)>
2019-05-04 21:48:41 +0100

    private properties

--- Trabalho 2/Trabalho 2/Travelpack.h
+++ Trabalho 2/Trabalho 2/Travelpack.h
@@ -1,8 +1,21 @@
 #pragma once
+
+#include <string>
+#include <vector>
+#include "Date.h"
+
 class Travelpack
 {
 public:
 	Travelpack();
 	~Travelpack();
+private:
+	size_t id;
+	std::vector<std::string> destinations;
+	Date begginning;
+	Date end;
+	double price_per_person;
+	size_t max_bought_tickets;
+	size_t bought_tickets;
 };
 
